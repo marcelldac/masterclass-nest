e1df5686df301cf53de20fc8af4480eb45cbab90
marcelldac <dacmarcell@gmail.com>
2023-10-13 23:56:48 -0300

    feat: add rocket member repository provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { PrismaService } from './database/prisma.service';
+import { PrismaRocketMembersRepository } from './repositories/prisma/prisma-rocket-members-repository';
+import {RocketMemberRepository} from './repositories/rocket-members-repository'
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: RocketMemberRepository,
+      useClass: PrismaRocketMembersRepository,
+    }
+
+  ],
 })
 export class AppModule {}
