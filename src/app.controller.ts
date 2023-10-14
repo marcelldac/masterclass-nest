5a0bec3b97604c10759f192ef5c0d93d94e4b3db
marcelldac <dacmarcell@gmail.com>
2023-10-13 23:22:40 -0300

    feat: add create team member method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import {randomUUID} from 'node:crypto';
 
-@Controller()
+import { PrismaService } from './database/prisma.service';
+import { CreateTeamMemberBody } from './dtos/create-team-member-body';
+
+@Controller('app')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  constructor(
+   private prisma: PrismaService, 
+  ){}
+  @Get('hello')
+  async getHello(@Body() body: CreateTeamMemberBody) {
+    const {name, function: memberFunction} = body;
+    const member = await this.prisma.rocketTeamMember.create({
+      data:{
+        id: randomUUID(),
+        name,
+        function: memberFunction
+      }
+    })
+    return {
+      member
+    };
   }
 }
